e426259e61f15274d040a5147840414b93d2e026
TY <47445484+R-YaTian@users.noreply.github.com>
2025-05-13 00:42:34 +0800

    Add files via upload

---
+++ dllmain.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+
+BOOL WINAPI DllMain(HINSTANCE hinst, DWORD reason, LPVOID reserved)
+{
+    switch (reason)
+    {
+    case DLL_PROCESS_ATTACH:
+        DisableThreadLibraryCalls(hinst);
+        break;
+
+    case DLL_PROCESS_DETACH:
+        if (reserved) break;
+        break;
+    }
+    return TRUE;
+}
